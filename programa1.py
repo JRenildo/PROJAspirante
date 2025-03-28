0bfcf070a0289a7117afbce28e0136b34dd51ccb
JRenildo <163761036+JRenildo@users.noreply.github.com>
2025-03-28 19:39:03 -0300

    Add files via upload

---
+++ programa1.py
@@ -0,0 +1,10 @@
+a = input("Digite seu nome")
+b = input("Nome do pruduto")
+c = float(input("Quantidade comprada"))
+d = int(input("O valor pago por item"))
+
+e = c*d
+
+print("Pedido confirmado: ", b)
+print("Valor total: ","R$", e)
+print("Obrigado pela preferência!")
